a8cb15e43990876a83fe786453672cc4a7d5ef09
Nicholas <nicholasluis@gmail.com>
2021-02-09 18:47:45 -0300

    fixed run script

--- run.sh
+++ run.sh
@@ -1,11 +1,52 @@
-#!/usr/bin/env bash
-
-export CB_NICKNAME=""
-export CB_IDENTD=""
-export CB_HOST=""
-export CB_CHANNEL=""
+#!/bin/bash
 
 
+lazy="1"
 
+
+if [ "$lazy" =  "1" ];
+  then
+  echo ""
+  echo ""
+  echo "you're a lazy and didnt edited run.sh with the variables to connect, please edit run.sh and set lazy variable as \"0\""
+  echo ""
+  echo ""
+  exit
+fi
+exit
+
+
+export cb_nickname="lamebot" #your nickname
+export cb_identd="itsame"   #your identd
+export cb_host="irc.lameserver.org" #server
+export cb_channel="##lamechannel" #channel
+
+
+
+#optional variables uncomment below if you need then
+
+
+#if you need to auth on server uncomment below and put your password
+#this is a secure measure that you dont put your passwd in any file of the bot
+#but still acessible with "echo $cb_user password"
+
+#export cb_user_passwd=""   
+
+#to use our weather command you need OpenWeather API key
+#uncomment below and put between ""
+
+#export open_weather_api=""
+
+
+#if you dont want to auth to nickerv just uncomment below and put 0
+
+
+#export cb_nickserv_auth=1
+
+
+
+while true
+do
 python run.py
-
+sleep 1
+done
