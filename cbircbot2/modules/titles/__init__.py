0291dce4481317ee84a8602982fa5e09f6614f1b
ryonagana <nicholasluis@gmail.com>
2021-10-14 15:56:04 -0300

    added youtube link support, youtube.com and youtu.be

--- cbircbot2/modules/titles/__init__.py
+++ cbircbot2/modules/titles/__init__.py
@@ -3,6 +3,7 @@ import requests
 import re
 import html
 from contextlib import suppress
+import json
 
 class titles(IrcModuleInterface):
     
@@ -30,36 +31,41 @@ class titles(IrcModuleInterface):
             return None
         
         return match.group()
+    
+    def _is_youtube_link(self, msg):
         
-    def on_message(self, *args, **kwargs):
-        message = kwargs["message"]
+        match = re.compile(r"(https:|http:)\/\/(?:w{3}\.)?(youtube\.com|youtu.be)\/(?:watch\?v\=)?(.+[aA-zZ0-9])", re.IGNORECASE)
+        search = match.search(msg)
+        if not search.groups():
+            return None
         
-        if not self._detect_protocol(message):
-            return
-            
-        link = self._extract_link(message) 
+        return search.groups()
         
+    def get_site_title(self, message):
+        link = self._extract_link(message)
+        req = None
+    
         if not link:
             return
-        
+    
         try:
             #blocking redirects avoid ifinite loops when a website is incorrectly redirected
             req = requests.get(link, allow_redirects=False, stream=True)
-    
+        
             if req.status_code != 200:
                 self.irc.msg_to_channel(self.irc.params.CHANNEL, f"{req.url} Invalid Response: {req.status_code}")
                 return
-                
+        
             try:
                 content_type = req.headers['content-type']
                 print(f"content-type: {content_type}")
-                
+            
                 if content_type.find("text/html") != -1:
                     html_element = req.text
                     title = html_element[ html_element.find('<title>') + 7 : html_element.find('</title>')]
                     self.irc.msg_to_channel(self.irc.params.CHANNEL, f"{html.unescape(title)}")
                     return
-                
+            
                 elif content_type.find("image/") != -1:
                     if int(req.headers['content-length']) > 100000:
                         self.irc.msg_to_channel(self.irc.params.CHANNEL, f"{link} took too long. aborting")
@@ -68,9 +74,55 @@ class titles(IrcModuleInterface):
                     return
             except IndexError:
                 print(f"{req.url} has incorrect context-type")
+    
+        except requests.exceptions.Timeout:
+            self.irc.msg_to_channel(self.irc.params.CHANNEL, f"{req.url} timed out")
+        except requests.HTTPError as http:
+            self.irc.msg_to_channel(self.irc.params.CHANNEL, f"{req.url} occurred a HTTP Exception Error: {http}")
+        return
+
+    
+    def get_youtube_data(self, data, link, *args, **kwargs):
+        req = None
+        line_prefix = None
+        try:
+            link_prefix = data[2]
+            link_oembed = f"https://www.youtube.com/oembed?url={link}&format=json"
+            req = requests.get(link_oembed, allow_redirects=False, stream=True)
+            
+            content_type = req.headers['content-type']
+            
+            if content_type.find("application/json") == -1:
+                self.irc.msg_to_channel(self.irc.params.CHANNEL, f"{req.url} Invalid Response: {req.status_code} is not a JSON")
+                
+            if req.status_code != 200:
+                self.irc.msg_to_channel(self.irc.params.CHANNEL, f"{req.url} Invalid Response: {req.status_code}")
+                return
+
+            json_text = json.loads(req.text)
+            
+            if "title" in json_text:
+                self.irc.msg_to_channel(self.irc.params.CHANNEL, f"Youtube: {json_text['title']}")
+
 
         except requests.exceptions.Timeout:
             self.irc.msg_to_channel(self.irc.params.CHANNEL, f"{req.url} timed out")
         except requests.HTTPError as http:
             self.irc.msg_to_channel(self.irc.params.CHANNEL, f"{req.url} occurred a HTTP Exception Error: {http}")
+        pass
+    
+    
+    def on_message(self, *args, **kwargs):
+        message = kwargs["message"]
+        
+        if not self._detect_protocol(message):
+            return
+        
+        link = self._is_youtube_link(message)
+        
+        if link:
+            self.get_youtube_data(link, message)
+            return
+        
+        self.get_site_title(message)
         return
